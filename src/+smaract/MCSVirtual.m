75f3c3d548c00bbbc94ee4f85087437769e75f6f
Chris Anderson from MET5Workstation <cnanderson@lbl.gov>
2022-11-02 20:24:55 -0700

    fixing zero indexing in virtual class

--- src/+smaract/MCSVirtual.m
+++ src/+smaract/MCSVirtual.m
@@ -17,17 +17,17 @@ classdef MCSVirtual < smaract.MCSAbstract
         
         %@param {uint32} absolute position to move to in nm
         function goToPositionAbsolute(this, u32Channel, u32Position)
-            this.u32Positions(u32Channel) = u32Position;
+            this.u32Positions(u32Channel + 1) = u32Position;
         end
         
        
         function findReferenceMark(this, u32Channel)
-            this.lIsReferenced(u32Channel) = true;
+            this.lIsReferenced(u32Channel + 1) = true;
         end
         
         
         function l = getIsReferenced(this, u32Channel)
-            l = this.lIsReferenced(u32Channel);
+            l = this.lIsReferenced(u32Channel + 1);
         end
             
             
@@ -36,7 +36,7 @@ classdef MCSVirtual < smaract.MCSAbstract
         end
            
         function u32Position = getPosition(this, u32Channel)
-            u32Position = this.u32Positions(u32Channel);
+            u32Position = this.u32Positions(u32Channel + 1);
         end
         
     end
--- test/test_MCSVirtual.m
+++ test/test_MCSVirtual.m
@@ -7,10 +7,6 @@ addpath(fullfile(cDir, '..', 'src'));
 
 %% Create, init, and open
 comm = smaract.MCSVirtual();
-comm.init();
-
-%% Get locator
-comm.getSystemLocator()
 
 %% Find reference
 comm.getIsReferenced(0)
@@ -19,9 +15,4 @@ comm.getIsReferenced(0)
 
 %% Move to position
 comm.goToPositionAbsolute(0, 5e6)
-comm.getStatusOfMovement(0)
 comm.getPosition(0)
-comm.getSensorEnabled();
-
-%% Close
-comm.closeSystem();
